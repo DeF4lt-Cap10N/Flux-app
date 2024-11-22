71d18334dadbb4f998e4821ab3b27c5f5e133bbb
akash1675 <captainakash1675@gmail.com>
2024-11-23 02:45:14 +0530

    add tailwind.config

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold'>welcome to flux app</h1>
+      <h1 className='text-5xl font-bold text-violet-300 font-robert-regular'>welcome to flux app</h1>
     </main>
   )
 }
