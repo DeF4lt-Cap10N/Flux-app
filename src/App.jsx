32a1dc684d06787fb951a019a4dea2841bb0eecb
akash1675 <captainakash1675@gmail.com>
2024-11-24 02:43:55 +0530

    add hero sec.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from './components/Hero'
+
+
 
 const App = () => {
   return (
-    <main>
-      <h1 className='text-5xl font-bold text-violet-300 font-robert-regular'>welcome to flux app</h1>
+    <main className='relative min-h-screen w-screen overflow-auto overflow-x-hidden'>
+      <Hero/>
     </main>
   )
 }
