3438cbffb8d8d7304ea3eb09e1bc43b150dcd1e8
akash1675 <captainakash1675@gmail.com>
2024-12-01 17:25:50 +0530

    add complete about sec.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
           end: '+=800 center',
           scrub: 0.5,
           pin: true,
-          pinSpacing: 'true'
+          pinSpacing: true
         }
       }
     )
@@ -26,7 +26,7 @@ const About = () => {
     clipAnimation.to('.mask-clip-path', {
       width: '100vw',
       height: '100vh',
-      borderRadius:0,
+      borderRadius: 0,
     })
 
   })
@@ -38,21 +38,20 @@ const About = () => {
       className='min-h-screen w-screen'>
       <div
         className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
-        <h2 className='font-general text-sm uppercase md:text-[10px]'>
+        <p className='font-general text-sm uppercase md:text-[10px]'>
           Welcome to Zentry
-        </h2>
-        <AnimatedTitle/>
-        <div
-          className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-          Disc<b>o</b>ver the world's<br /> l<b>a</b>rgest shared adventure
-        </div>
+        </p>
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's<br/> largest shared <b>a</b>dventure" containerClass="mt-5 !text-black text-center"
+        />
         <div
           className='about-subtext'>
           <p>
             The Game of Games begins-your life, now an epic MMORPG
           </p>
-          <p>
-            Zenty unites every player from countless games and platfroms
+          <p className="text-gray-500">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
           </p>
         </div>
       </div>
