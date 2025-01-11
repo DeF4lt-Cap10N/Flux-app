9b11943a8465862d0cb1f35a8c9f4ff661dce895
akash1675 <captainakash1675@gmail.com>
2025-01-11 20:17:07 +0530

    add story page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
+import Story from './components/Story'
 
 
 
@@ -13,7 +14,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Features/>
-     
+     <Story/>
     </main>
   )
 }
